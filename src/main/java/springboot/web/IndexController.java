e70372e528c90125ed365b276c9e53e6cab3275a
chlqls <chlqls212@gmail.com>
2022-09-23 15:18:26 +0900

    0923

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package springboot.web;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import springboot.service.posts.PostsService;
+import springboot.web.dto.PostsResponseDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,6 +26,13 @@ public class IndexController {
         return "posts-save";
     }
 
+    @GetMapping("/posts/update/{id")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
 
 
 }
