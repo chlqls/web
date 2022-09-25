f6db8a1a92bf2ce75866d8dfd1b8545d103d86e7
chlqls <chlqls212@gmail.com>
2022-09-25 10:56:58 +0900

    0925(2)

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springboot.config.auth.LoginUser;
 import springboot.config.auth.dto.SessionUser;
 import springboot.service.posts.PostsService;
 import springboot.web.dto.PostsResponseDto;
@@ -17,12 +18,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
